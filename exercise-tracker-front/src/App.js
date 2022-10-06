b4ebbc575db63ed323fa7c1dcb6ea765ba38e202
Harsh Kumar Singh <harshkumarsinghpawar@gmail.com>
2022-10-06 20:28:39 +0530

    project complete 2.0

--- exercise-tracker-front/src/App.js
+++ exercise-tracker-front/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
@@ -9,29 +9,17 @@ import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
-  const [user, setUser] = React.useState(null);
-
-  async function login(user = null) {
-    setUser(user);
-  }
-
-  async function logout() {
-    setUser(null);
-  }
-
   return (
-    <div>
-      <Route>
-        <div className="container">
-          <Navbar />
-          <br />
-          <Route path="/" exact component={ExercisesList} />
-          <Route path="/edit/:id" component={EditExercise} />
-          <Route path="/create" component={CreateExercise} />
-          <Route path="/user" component={CreateUser} />
-        </div>
-      </Route>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
--- exercise-tracker-front/src/index.js
+++ exercise-tracker-front/src/index.js
@@ -9,9 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-window.React1 = require("react");
-
-// Add this in your component file
-require("react-dom");
-window.React2 = require("react");
-console.log(window.React1 === window.React2);
